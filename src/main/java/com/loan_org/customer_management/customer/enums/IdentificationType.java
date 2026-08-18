a73c86fcc654750bec829b65c6c08f53e8f865a0
Aman Raj <billygates124356@gmail.com>
2026-08-18 23:20:36 +0530

    identity types

--- src/main/java/com/loan_org/customer_management/customer/enums/IdentificationType.java
+++ src/main/java/com/loan_org/customer_management/customer/enums/IdentificationType.java
@@ -1,16 +1,18 @@
 package com.loan_org.customer_management.customer.enums;
 
+/**
+ * Defines the identification type for the customer identity 
+ * verification. These enums are important.
+ * 
+ * @author amanfoundongithub
+ * @version 1.0.0
+ * 
+ */
 public enum IdentificationType {
-
     PAN,
-
     AADHAAR,
-
     PASSPORT,
-
     VOTER_ID,
-
     DRIVING_LICENSE,
-
     OTHER
 }
